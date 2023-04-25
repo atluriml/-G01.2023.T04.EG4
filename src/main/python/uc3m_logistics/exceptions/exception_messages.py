1252819cdb1e1e068d1a308ed4f5cb2ef2bcad95
sarah-eliz <sarah.e.hoover.1@vanderbilt.edu>
2023-04-25 15:52:10 +0200

    added to exception_messages.py

---
+++ src/main/python/uc3m_logistics/exceptions/exception_messages.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+class ExceptionMessages():
+
+    ORDERS_DATA_MANIPULATED = "'Orders' data have been manipulated"
+
+    """ATTRIBUTES"""
+    ADDRESS_NOT_VALID = "address is not valid"
+    EAN13_NOT_VALID = "Invalid EAN13 code string"
+    EAN13_CONTROL_DIGIT_NOT_VALID = " Invalid EAN13 control digit"
+    EMAIL_NOT_VALID = "contact email is not valid"
+    ORDER_ID_NOT_VALID = "order id is not valid"
+    ORDER_TYPE_NOT_VALID = "order_type is not valid"
+    PHONE_NUMBER_NOT_VALID = "phone number is not valid"
+    TRACKING_CODE_NOT_VALID = "tracking code is not valid"
+    ZIP_CODE_FORMAT_NOT_VALID = "zip code format is not valid"
+    ZIP_CODE_NOT_VALID = "zip code is not valid"
+    ORDER_ID_NOT_VALID = "OrderIS is not present"
+    CONTACT_EMAIL_NOT_PRESENT = "ContactEmail is not present"
+
+    """ORDER DELIVERY"""
+
+    TRACKING_CODE_IS_NOT_FOUND = "tracking_code is not found"
+    TODAY_IS_NOT_DELIVERY_DATE = "Today is not the delivery date"
