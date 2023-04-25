25edb23ab8ab0e4f4ee8902e504733f1578558a5
krislyn dorsey <83076538+krislyndorsey@users.noreply.github.com>
2023-04-25 15:23:23 +0200

    small pylint updates for validation module

--- src/main/python/uc3m_logistics/stores/json_store.py
+++ src/main/python/uc3m_logistics/stores/json_store.py
@@ -1,4 +1,6 @@
-
+import json
+from src.main.python.uc3m_logistics.order_management_exception import OrderManagementException
+from abc import abstractmethod, ABC
 
 class JsonStore(ABC, metaclass=FinalMeta):
     _FILE_PATH = None
@@ -8,3 +10,13 @@ class JsonStore(ABC, metaclass=FinalMeta):
 
     def load(self):
         try:
+            with open(self._FILE_PATH, "r", encoding="utf-8", newline="") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            # file is not found , so init my data
+            data = []
+            with open(self._FILE_PATH, "w", encoding="utf-8", newline="") as file:
+                json.dump(data, file, indent=2)
+        except json.JSONDecoder as ex:
+            raise OrderManagementException(ExceptionMessages.JSON_DECODE_ERROR.value) from ex
+        return data
