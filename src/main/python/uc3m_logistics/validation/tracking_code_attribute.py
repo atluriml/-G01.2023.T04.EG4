c35d48d4fdda28194d4fea897e576dea4a19d892
krislyn dorsey <83076538+krislyndorsey@users.noreply.github.com>
2023-04-28 01:43:35 +0200

    no more pylint errors

--- src/main/python/uc3m_logistics/validation/tracking_code_attribute.py
+++ src/main/python/uc3m_logistics/validation/tracking_code_attribute.py
@@ -2,11 +2,14 @@
 
 import re
 from uc3m_logistics.exceptions.order_management_exception import OrderManagementException
-from .attribute import Attribute
+from uc3m_logistics.validation.attribute import Attribute
+
+
 class TrackingCodeAttribute(Attribute):
     """Tracking Code Validation Class"""
 
     tracking_code_regex = re.compile(r"[0-9a-fA-F]{64}$")
+
     def validate(self, value):
         """tracking code validate function"""
         res = self.tracking_code_regex.fullmatch(value)
