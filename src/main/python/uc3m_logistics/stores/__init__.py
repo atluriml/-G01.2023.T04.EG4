cc9d480197a1f44f83f43247c4f3ba96012b9906
sarah-eliz <sarah.e.hoover.1@vanderbilt.edu>
2023-04-27 14:52:45 +0200

    changed a few init imports

--- src/main/python/uc3m_logistics/stores/__init__.py
+++ src/main/python/uc3m_logistics/stores/__init__.py
@@ -1,4 +1,4 @@
 from .json_store import JsonStore
-#from .order_request_store import OrderRequestStore
+from .order_request_store import OrderRequestStore
 from .order_shipping_store import OrderShippingStore
-#from .order_delivery_store import OrderDeliveryStore
+from .order_delivery_store import OrderDeliveryStore
