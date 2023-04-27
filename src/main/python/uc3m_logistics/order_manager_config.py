f57a911fadb17cd16c253bcb4fa7f043b14016a7
krislyn dorsey <83076538+krislyndorsey@users.noreply.github.com>
2023-04-28 01:46:10 +0200

    no more pylint errors

--- src/main/python/uc3m_logistics/order_manager_config.py
+++ src/main/python/uc3m_logistics/order_manager_config.py
@@ -4,7 +4,7 @@ from enum import Enum
 
 
 # path to src folder + "/JsonFiles/
-JSON_FILES_PATH = os.path.join(os.path.dirname(__file__),"../../../JsonFiles/")
+JSON_FILES_PATH = os.path.join(os.path.dirname(__file__), "../../../JsonFiles/")
 
 JSON_FILES_RF2_PATH = JSON_FILES_PATH + "/FR2InputFiles/"
 
