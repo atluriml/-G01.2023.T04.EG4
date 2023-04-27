6e21b37ec49d7fbab986db4d737d44524d8cdf25
krislyn dorsey <83076538+krislyndorsey@users.noreply.github.com>
2023-04-28 01:20:04 +0200

    no more pylint errors

--- src/main/python/uc3m_logistics/models/order_shipping.py
+++ src/main/python/uc3m_logistics/models/order_shipping.py
@@ -23,8 +23,8 @@ class OrderShipping:
             delivery_days = 7
         else:
             delivery_days = 1
-        # timestamp is represneted in seconds.microseconds
-        # __delivery_day must be expressed in senconds to be added to the timestap
+        # timestamp is represented in seconds.microseconds
+        # __delivery_day must be expressed in seconds to be added to the timestamp
         self.__delivery_day = self.__issued_at + (delivery_days * 24 * 60 * 60)
         self.__tracking_code = hashlib.sha256(self.__signature_string().encode()).hexdigest()
 
@@ -49,8 +49,8 @@ class OrderShipping:
     def __signature_string(self):
         """Composes the string to be used for generating the tracking_code"""
         return "{alg:" + self.__alg + ",typ:" + self.__type + ",order_id:" + \
-               self.__order_id + ",issuedate:" + str(self.__issued_at) + \
-               ",deliveryday:" + str(self.__delivery_day) + "}"
+               self.__order_id + ", issue date:" + str(self.__issued_at) + \
+               ",delivery day:" + str(self.__delivery_day) + "}"
 
     @property
     def product_id(self):
@@ -58,42 +58,42 @@ class OrderShipping:
         return self.__product_id
 
     @product_id.setter
-    def product_id( self, value ):
+    def product_id(self, value):
         self.__product_id = value
 
     @property
-    def order_id( self ):
+    def order_id(self):
         """Property that represents the order_id"""
         return self.__order_id
 
     @order_id.setter
-    def order_id( self, value ):
+    def order_id(self, value):
         self.__order_id = value
 
     @property
-    def email( self ):
+    def email(self):
         """Property that represents the email of the client"""
         return self.__delivery_email
 
     @email.setter
-    def email( self, value ):
+    def email(self, value):
         self.__delivery_email = value
 
     @property
-    def tracking_code( self ):
+    def tracking_code(self):
         """returns the tracking code"""
         return self.__tracking_code
 
     @property
-    def issued_at( self ):
+    def issued_at(self):
         """Returns the issued at value"""
         return self.__issued_at
 
     @issued_at.setter
-    def issued_at( self, value ):
+    def issued_at(self, value):
         self.__issued_at = value
 
     @property
-    def delivery_day( self ):
+    def delivery_day(self):
         """Returns the delivery day for the order"""
         return self.__delivery_day
