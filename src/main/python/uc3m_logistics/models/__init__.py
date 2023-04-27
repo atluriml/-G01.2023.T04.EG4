278cb31bdf8bbc78650d7a34ad70c8ce7904a83d
Megana Atluri <megana.l.atluri@vanderbilt.edu>
2023-04-27 16:46:48 +0200

    updated init file for models

---
+++ src/main/python/uc3m_logistics/models/__init__.py
@@ -0,0 +1,5 @@
+from .order_shipping import OrderShipping
+from .order_request import OrderRequest
+from .order_delivery import OrderDelivery
+from .order_manager import OrderManager
+from .send_product_input import SendProductInput
