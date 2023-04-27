0e791e1cb0290bf55d43cf7ca0ca3d8e585f9443
krislyn dorsey <83076538+krislyndorsey@users.noreply.github.com>
2023-04-28 01:44:21 +0200

    no more pylint errors

--- src/main/python/uc3m_logistics/validation/zip_code_attribute.py
+++ src/main/python/uc3m_logistics/validation/zip_code_attribute.py
@@ -1,7 +1,8 @@
 """Zipcode Attribute Module"""
 
 from uc3m_logistics.exceptions.order_management_exception import OrderManagementException
-from .attribute import Attribute
+from uc3m_logistics.validation.attribute import Attribute
+
 
 class ZipCodeAttribute(Attribute):
     """Zipcode Attribute Class"""
@@ -9,7 +10,7 @@ class ZipCodeAttribute(Attribute):
     def validate(self, value):
         """validates zipcode attribute"""
         if value.isnumeric() and len(value) == 5:
-            if (int(value) > 52999 or int(value) < 1000):
+            if int(value) > 52999 or int(value) < 1000:
                 raise OrderManagementException("zip_code is not valid")
         else:
             raise OrderManagementException("zip_code format is not valid")
